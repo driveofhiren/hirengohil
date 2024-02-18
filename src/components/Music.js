d1d7b0d8d143a9f040eb964fb1b4b561f753a94e
Hiren Gohil <50377062+driveofhiren@users.noreply.github.com>
2024-02-18 12:18:00 -0500

    18 2

--- src/components/Music.js
+++ src/components/Music.js
@@ -31,13 +31,14 @@ export default class Music extends Component {
             </div>
             <h1><span>Listen whole Discography</span></h1>
             <div class="player-container">
-            <ReactPlayer
-        url="https://soundcloud.com/spirittoy/sets/spirit-toy-hir-n?si=0224b45f02db453b808e2c4c83491e9c&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing"
-      /></div>
+            <iframe width="70%" height="450px" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1653131743&color=%23040460&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe><div ><a href="https://soundcloud.com/spirittoy" target="_blank" ></a><a href="https://soundcloud.com/spirittoy/sets/spirit-toy-hir-n" target="_blank"></a></div></div>
        <h1><span>Videos</span></h1>
       <div class="player-container">
-      <iframe width="800px" height="450px" src="https://www.youtube.com/embed/videoseries?si=jh1EqnRja0KYdLbh&list=PL1t6M0BZsk-SQUiaJHuA74pafr0Szq2NZ" title="Spirit Toy Music" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      <iframe width="70%" height="450px" src="https://www.youtube.com/embed/videoseries?si=jh1EqnRja0KYdLbh&list=PL1t6M0BZsk-SQUiaJHuA74pafr0Szq2NZ" title="Spirit Toy Music" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
       </div>
+      <h1><span>Artist Radio</span></h1>
+            <div className="player-container">
+            <iframe src="https://open.spotify.com/embed/playlist/37i9dQZF1E4zQRNpLFRvji?utm_source=generator&theme=0" width="70%" height="400px" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>            </div>
           </div>
         </div>
       </section>
