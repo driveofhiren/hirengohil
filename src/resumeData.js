d3df41b84bf64e879ae8f324fc9f2cdd5d367eea
Hiren Gohil <50377062+driveofhiren@users.noreply.github.com>
2024-05-03 22:55:00 -0400

    addapp

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,298 +1,299 @@
 const resumeData = {
-  "imagebaseurl": "https://driveofhiren.github.io/",
-  "name": "Hiren Gohil",
-  "role": "Web Developer, Music Producer, Graphic Designer",
-  "skypeid": "Your skypeid",
-  "cv":"https://drive.google.com/file/d/1s5KAkCp5Oa4t-nz1BMXVxuhjjgk1nfXF/view?usp=drive_link",
-  "roleDescription": "I am a skilled professional, adept in the realms of web development, music production, and graphic design, seamlessly blending my creative instincts with technical expertise.",
-  "socialLinks": [
-    {
-      "name": "linkedin",
-      "url": "https://www.linkedin.com/in/hiren-gohil-362356142/",
-      "className": "fab fa-linkedin"
-    },
-    {
-      "name": "github",
-      "url": "https://github.com/driveofhiren",
-      "className": "fab fa-github"
-    },
-    {
-      "name": "twitter",
-      "url": "https://twitter.com/spirit_toy",
-      "className": "fab fa-twitter"
-    },
-    {
-      "name": "instagram",
-      "url": "https://www.instagram.com/h.gohil/",
-      "className": "fab fa-instagram"
-    },
-    {
-      "name": "soundcloud",
-      "url": "https://soundcloud.com/spirittoy",
-      "className": "fab fa-soundcloud"
-    },
-    {
-      "name": "youtube",
-      "url": "https://www.youtube.com/channel/UCRN6zHj_YuzDrH9qTw6FMIQ",
-      "className": "fab fa-youtube"
-    },
-    {
-      "name": "spotify",
-      "url": "https://open.spotify.com/artist/4G4Ko6bMdUf1hADOhCkd4c",
-      "className": "fab fa-spotify"
-    },
-    {
-      "name": "instagram",
-      "url": "https://www.instagram.com/spirittoymusic/",
-      "className": "fab fa-instagram"
-    },
-  ],
-  
-  "aboutme": "I'm Hiren Yogeshbhai Gohil, a tech enthusiast with a Bachelor's in Information & Technology from SVIT, Vasad, India, and currently pursuing Web Development at Conestoga College. I have a diverse background, having worked as a Technician, Graphic Designer, and Music Producer for multiple record labels. Let's innovate together!",
-  "address": "F-307 275 Larch St, Waterloo, ON N2L 3R2, CA",
-  "email": "hirengohil7792@gmail.com",
-  "phone": "+1-437-799-3689",
-  "instagramId" : "h.gohil",
-  "soundcloudId" : "spirittoy",
-  "youtubeId" : "@spirittoy5420",
-  "linkedinId" : "hiren-gohil-362356142",
-  "spotifyId" : "4G4Ko6bMdUf1hADOhCkd4c",
-  "website": "",
-  "education": [
-    {
-      "UniversityName": "Conestoga College",
-      "specialization": "Web Development",
-      "MonthOfPassing": "April",
-      "YearOfPassing": "2023",
-      "Achievements": "8.63"
-    },
-    {
-      "UniversityName": "SVIT, Vasad",
-      "specialization": "Information & Technology",
-      "MonthOfPassing": "July",
-      "YearOfPassing": "2020",
-      "Achievements": "8.2 GPA"
-    },
-    {
-      "UniversityName": "SVIT, Vasad",
-      "specialization": "Information & Technology",
-      "MonthOfPassing": "May",
-      "YearOfPassing": "2020",
-      "Achievements": "Bachelor of Engineering"
-    },
-    {
-      "UniversityName": "ST John School",
-      "specialization": "SSC",
-      "MonthOfPassing": "Year",
-      "YearOfPassing": "2014",
-      "Achievements": ""
-    },
-    {
-      "UniversityName": "Zenith High School",
-      "specialization": "HSC",
-      "MonthOfPassing": "Year",
-      "YearOfPassing": "2016",
-      "Achievements": ""
-    }
-  ],
-  "work": [
-    {
-      "CompanyName": "Shivam Computer",
-      "specialization": "Technician",
-      "MonthOfLeaving": "April",
-      "YearOfLeaving": "2014",
-      "Achievements": ""
-    },
-    {
-      "CompanyName": "National/College Level Tech-Fest",
-      "specialization": "Graphic Designer",
-      "MonthOfLeaving": "Feb",
-      "YearOfLeaving": "2019",
-      "Achievements": "I served as a Graphic Designer for National and College-level Tech-Fests from 2017 to 2019, where I crafted visually compelling designs, including posters, cards, brochures, certificates, and various documents, to enhance the overall aesthetic and communication of the events."
-    },
-    {
-      "CompanyName": "Numerator",
-      "specialization": "Data Classification Associate",
-      "MonthOfLeaving": "Present",
-      "YearOfLeaving": "",
-      "Achievements": "50+ WPM Speed"
-    },
-    {
-      "CompanyName": "Popeyes, Aunty’s Kitchen",
-      "specialization": "Cook",
-      "MonthOfLeaving": "Present",
-      "YearOfLeaving": "",
-      "Achievements": ""
-    }
-  ],
-  "skillsDescription": "Your skills here",
-  "skills": [
-    {
-      "skillname": "HTML5/CSS",
-      "level": 95
-    },
-    {
-      "skillname": "React.js",
-      "level": 80
-    },
-    {
-      "skillname": "Python, Java, C++/C#",
-      "level":90
-    },
-    {
-      "skillname": "Node.js, Express.js",
-      "level": 80
-    },
-    {
-      "skillname": "SQL/MongoDB/Firebase",
-      "level": 90
-    },
-    {
-      "skillname": "GraphQL",
-      "level": 90
-    },
-    {
-      "skillname": "PHP",
-      "level": 95
-    },
-    {
-      "skillname": "ASP.NET",
-      "level": 80
-    },
-    {
-      "skillname": "Adobe Photoshop",
-      "level": 90
-    },
-    {
-      "skillname": "Adobe Illustrator",
-      "level": 85
-    },
-    {
-      "skillname": "FL Studio",
-      "level": 95
-    },
-    {
-      "skillname": "Adobe Premiere",
-      "level": 85
-    },
-    {
-      "skillname": "Adobe Audition",
-      "level": 85
-    },
-    {
-      "skillname": "Adobe After Effects",
-      "level": 80
-    },
-    {
-      "skillname": "Microsoft Office Suit",
-      "level": 85
-    },
-  ],
-  "portfolio": [
-    {
-      "name": "Tesla Mobile",
-      "description": "Web design",
-      "imgurl": "https://github.com/driveofhiren/tesla-mobile-web-design"
-    },
-    {
-      "name": "Toronto Tourism",
-      "description": "Web Design",
-      "imgurl": "https://github.com/driveofhiren/toronto"
-    },
-    {
-      "name": "Employee Management System",
-      "description": "React and Mysql",
-      "imgurl": "https://github.com/driveofhiren/ems"
-    },
-    {
-      "name": "Desk E-commerce Application",
-      "description": "PHP",
-      "imgurl": "https://github.com/driveofhiren/ecomphp"
-    },
-    {
-      "name": "Book Store",
-      "description": "ASP.NET",
-      "imgurl": "https://github.com/driveofhiren/Book-Store"
-    },
-    {
-      "name": "Multiplayer Card Game",
-      "description": "React and Node.js",
-      "imgurl": "https://github.com/driveofhiren/cardgame"
-    },
-    {
-      "name": "Database design Car Services",
-      "description": "Database",
-      "imgurl": "https://github.com/driveofhiren/project-car-services"
-    },
-    {
-      "name": "Budget Web application",
-      "description": "JavaScript",
-      "imgurl": "https://github.com/driveofhiren/Budget-Application"
-    },
-    {
-      "name": "Cosmetics Website",
-      "description": "HTML/CSS",
-      "imgurl": "https://github.com/driveofhiren/Cosmeticd"
-    },
-    {
-      "name": "Bakery Website",
-      "description": "HTML/CSS",
-      "imgurl": "https://github.com/driveofhiren/bakery"
-    },
-    {
-      "name": "Musicial Artist Website",
-      "description": "HTML/CSS",
-      "imgurl": "https://github.com/driveofhiren/Artist-Website"
-    }
-  ],
-  "music": [
-    {
-      "id": 1,
-      "name": "Mahiya",
-      "description": "Description for Track 1",
-      "imgurl": "https://open.spotify.com/album/3OVfehhI2muqHQIc4r3wae?si=sfi7XUhiRi2k0kPKp_-dpA"
-    },
-    {
-      "id": 2,
-      "name": "Masters",
-      "description": "Description for Track 2",
-      "imgurl": "https://open.spotify.com/track/1YzO9G2ubLqj6tBjWfY2jG?si=a337a283b9fe4298"
-    },
-    {
-      "id": 3,
-      "name": "Masters 2",
-      "description": "Description for Track 1",
-      "imgurl": "https://open.spotify.com/track/49XBPjiXJqMNTtPhZIclwR?si=623fedc5d7524d9e"
-    },
-    {
-      "id": 4,
-      "name": "Tranquil",
-      "description": "Description for Track 2",
-      "imgurl": "https://open.spotify.com/track/0kY03hvGKov6olyWyg7wey?si=e8457e29a63149ae"
-    },
-    {
-      "id": 5,
-      "name": "Ashle",
-      "description": "Description for Track 1",
-      "imgurl": "https://open.spotify.com/track/6ct4xQaoDpwIlJuKtvVcCL"
-    },
-    {
-      "id": 6,
-      "name": "Osho Wave",
-      "description": "Description for Track 2",
-      "imgurl": "https://open.spotify.com/album/20j9Vc4dMVlP3cyabv14D1?si=9i59uigKQVazTvNjm3q7jw"
-    },
-  ],
-  "testimonials": [
-    {
-      "description": "This is a sample testimonial",
-      "name": "Some technical guy"
-    },
-    {
-      "description": "This is a sample testimonial",
-      "name": "Some technical guy"
-    }
-  ]
-};
+	imagebaseurl: 'https://driveofhiren.github.io/',
+	name: 'Hiren Gohil',
+	role: 'Web Developer, Music Producer, Graphic Designer',
+	skypeid: 'Your skypeid',
+	cv: 'https://drive.google.com/file/d/1s5KAkCp5Oa4t-nz1BMXVxuhjjgk1nfXF/view?usp=drive_link',
+	roleDescription:
+		'I am a skilled professional, adept in the realms of web development, music production, and graphic design, seamlessly blending my creative instincts with technical expertise.',
+	socialLinks: [
+		{
+			name: 'linkedin',
+			url: 'https://www.linkedin.com/in/hiren-gohil-362356142/',
+			className: 'fab fa-linkedin',
+		},
+		{
+			name: 'github',
+			url: 'https://github.com/driveofhiren',
+			className: 'fab fa-github',
+		},
+		{
+			name: 'twitter',
+			url: 'https://twitter.com/spirit_toy',
+			className: 'fab fa-twitter',
+		},
+		{
+			name: 'instagram',
+			url: 'https://www.instagram.com/h.gohil/',
+			className: 'fab fa-instagram',
+		},
+		{
+			name: 'soundcloud',
+			url: 'https://soundcloud.com/spirittoy',
+			className: 'fab fa-soundcloud',
+		},
+		{
+			name: 'youtube',
+			url: 'https://www.youtube.com/channel/UCRN6zHj_YuzDrH9qTw6FMIQ',
+			className: 'fab fa-youtube',
+		},
+		{
+			name: 'spotify',
+			url: 'https://open.spotify.com/artist/4G4Ko6bMdUf1hADOhCkd4c',
+			className: 'fab fa-spotify',
+		},
+		{
+			name: 'instagram',
+			url: 'https://www.instagram.com/spirittoymusic/',
+			className: 'fab fa-instagram',
+		},
+	],
 
-export default resumeData;
+	aboutme:
+		"I'm Hiren Yogeshbhai Gohil, a tech enthusiast with a Bachelor's in Information & Technology from SVIT, Vasad, India, and currently pursuing Web Development at Conestoga College. I have a diverse background, having worked as a Technician, Graphic Designer, and Music Producer for multiple record labels. Let's innovate together!",
+	address: 'F-307 275 Larch St, Waterloo, ON N2L 3R2, CA',
+	email: 'hirengohil7792@gmail.com',
+	phone: '+1-437-799-3689',
+	instagramId: 'h.gohil',
+	soundcloudId: 'spirittoy',
+	youtubeId: '@spirittoy5420',
+	linkedinId: 'hiren-gohil-362356142',
+	spotifyId: '4G4Ko6bMdUf1hADOhCkd4c',
+	website: '',
+	education: [
+		{
+			UniversityName: 'Conestoga College',
+			specialization: 'Web Development',
+			MonthOfPassing: 'April',
+			YearOfPassing: '2023',
+			Achievements: '3.67 GPA',
+		},
+		{
+			UniversityName: 'SVIT, Vasad',
+			specialization: 'Information & Technology',
+			MonthOfPassing: 'July',
+			YearOfPassing: '2020',
+			Achievements: '3.12 GPA',
+		},
+		{
+			UniversityName: 'ST John School',
+			specialization: 'SSC',
+			MonthOfPassing: 'Year',
+			YearOfPassing: '2014',
+			Achievements: '',
+		},
+		{
+			UniversityName: 'Zenith High School',
+			specialization: 'HSC',
+			MonthOfPassing: 'Year',
+			YearOfPassing: '2016',
+			Achievements: '',
+		},
+	],
+	work: [
+		{
+			CompanyName: 'Shivam Computer',
+			specialization: 'Technician',
+			MonthOfLeaving: 'April',
+			YearOfLeaving: '2014',
+			Achievements: '',
+		},
+		{
+			CompanyName: 'National/College Level Tech-Fest',
+			specialization: 'Graphic Designer',
+			MonthOfLeaving: 'Feb',
+			YearOfLeaving: '2019',
+			Achievements:
+				'I served as a Graphic Designer for National and College-level Tech-Fests from 2017 to 2019, where I crafted visually compelling designs, including posters, cards, brochures, certificates, and various documents, to enhance the overall aesthetic and communication of the events.',
+		},
+		{
+			CompanyName: 'Numerator',
+			specialization: 'Data Classification Associate',
+			MonthOfLeaving: 'Present',
+			YearOfLeaving: '',
+			Achievements: '50+ WPM Speed',
+		},
+		{
+			CompanyName: 'Popeyes, Aunty’s Kitchen',
+			specialization: 'Cook',
+			MonthOfLeaving: 'Present',
+			YearOfLeaving: '',
+			Achievements: '',
+		},
+	],
+	skillsDescription: 'Your skills here',
+	skills: [
+		{
+			skillname: 'HTML5/CSS',
+			level: 95,
+		},
+		{
+			skillname: 'React.js',
+			level: 80,
+		},
+		{
+			skillname: 'Python, Java, C++/C#',
+			level: 90,
+		},
+		{
+			skillname: 'Node.js, Express.js',
+			level: 80,
+		},
+		{
+			skillname: 'SQL/MongoDB/Firebase',
+			level: 90,
+		},
+		{
+			skillname: 'GraphQL',
+			level: 90,
+		},
+		{
+			skillname: 'PHP',
+			level: 95,
+		},
+		{
+			skillname: 'ASP.NET',
+			level: 80,
+		},
+		{
+			skillname: 'Adobe Photoshop',
+			level: 90,
+		},
+		{
+			skillname: 'Adobe Illustrator',
+			level: 85,
+		},
+		{
+			skillname: 'FL Studio',
+			level: 95,
+		},
+		{
+			skillname: 'Adobe Premiere',
+			level: 85,
+		},
+		{
+			skillname: 'Adobe Audition',
+			level: 85,
+		},
+		{
+			skillname: 'Adobe After Effects',
+			level: 80,
+		},
+		{
+			skillname: 'Microsoft Office Suit',
+			level: 85,
+		},
+	],
+	portfolio: [
+		{
+			name: 'Tesla Mobile',
+			description: 'Web design',
+			imgurl: 'https://github.com/driveofhiren/tesla-mobile-web-design',
+		},
+		{
+			name: 'Toronto Tourism',
+			description: 'Web Design',
+			imgurl: 'https://github.com/driveofhiren/toronto',
+		},
+		{
+			name: 'Employee Management System',
+			description: 'React and Mysql',
+			imgurl: 'https://github.com/driveofhiren/ems',
+		},
+		{
+			name: 'Desk E-commerce Application',
+			description: 'PHP',
+			imgurl: 'https://github.com/driveofhiren/ecomphp',
+		},
+		{
+			name: 'Book Store',
+			description: 'ASP.NET',
+			imgurl: 'https://github.com/driveofhiren/Book-Store',
+		},
+		{
+			name: 'Multiplayer Card Game',
+			description: 'React and Node.js',
+			imgurl: 'https://github.com/driveofhiren/cardgame',
+		},
+		{
+			name: 'Database design Car Services',
+			description: 'Database',
+			imgurl: 'https://github.com/driveofhiren/project-car-services',
+		},
+		{
+			name: 'Budget Web application',
+			description: 'JavaScript',
+			imgurl: 'https://github.com/driveofhiren/Budget-Application',
+		},
+		{
+			name: 'Cosmetics Website',
+			description: 'HTML/CSS',
+			imgurl: 'https://github.com/driveofhiren/Cosmeticd',
+		},
+		{
+			name: 'Bakery Website',
+			description: 'HTML/CSS',
+			imgurl: 'https://github.com/driveofhiren/bakery',
+		},
+		{
+			name: 'Musicial Artist Website',
+			description: 'HTML/CSS',
+			imgurl: 'https://github.com/driveofhiren/Artist-Website',
+		},
+		{
+			name: 'Astrology Community App(*)',
+			description: 'React.js/Node.js',
+			imgurl: '',
+		},
+	],
+	music: [
+		{
+			id: 1,
+			name: 'Mahiya',
+			description: 'Description for Track 1',
+			imgurl: 'https://open.spotify.com/album/3OVfehhI2muqHQIc4r3wae?si=sfi7XUhiRi2k0kPKp_-dpA',
+		},
+		{
+			id: 2,
+			name: 'Masters',
+			description: 'Description for Track 2',
+			imgurl: 'https://open.spotify.com/track/1YzO9G2ubLqj6tBjWfY2jG?si=a337a283b9fe4298',
+		},
+		{
+			id: 3,
+			name: 'Masters 2',
+			description: 'Description for Track 1',
+			imgurl: 'https://open.spotify.com/track/49XBPjiXJqMNTtPhZIclwR?si=623fedc5d7524d9e',
+		},
+		{
+			id: 4,
+			name: 'Tranquil',
+			description: 'Description for Track 2',
+			imgurl: 'https://open.spotify.com/track/0kY03hvGKov6olyWyg7wey?si=e8457e29a63149ae',
+		},
+		{
+			id: 5,
+			name: 'Ashle',
+			description: 'Description for Track 1',
+			imgurl: 'https://open.spotify.com/track/6ct4xQaoDpwIlJuKtvVcCL',
+		},
+		{
+			id: 6,
+			name: 'Osho Wave',
+			description: 'Description for Track 2',
+			imgurl: 'https://open.spotify.com/album/20j9Vc4dMVlP3cyabv14D1?si=9i59uigKQVazTvNjm3q7jw',
+		},
+	],
+	testimonials: [
+		{
+			description: 'This is a sample testimonial',
+			name: 'Some technical guy',
+		},
+		{
+			description: 'This is a sample testimonial',
+			name: 'Some technical guy',
+		},
+	],
+}
+
+export default resumeData
