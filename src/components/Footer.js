26521d02acad4dc5251600106ed1aa4c4f46bc81
Hiren Gohil <50377062+driveofhiren@users.noreply.github.com>
2024-06-08 09:29:33 -0400

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,35 @@
-import React, { Component } from 'react';
+import React from 'react'
 
-export default class Footer extends Component {
-  render() {
-    return (
-      
-      <footer>
-        <div>
-          <div>
-            <p>&copy; 2024 Hiren Gohil. All rights reserved.</p>
-          </div>
-        </div>
-      </footer>
-    );
-  }
-}
+const Footer = ({ resumeData }) => (
+	<footer style={{ backgroundColor: '#1B3A4B', padding: '20px 0px 5px 0px' }}>
+		<div style={{ textAlign: 'center', color: '#ffffff' }}>
+			<p>&copy; 2024 {resumeData.name}. All rights reserved.</p>
+			<div>
+				<ul
+					className="social"
+					style={{ listStyle: 'none', padding: 0 }}
+				>
+					{resumeData.socialLinks &&
+						resumeData.socialLinks.map((item) => (
+							<li
+								key={item.name}
+								style={{ display: 'inline', margin: '0 10px' }}
+							>
+								<a href={item.url}>
+									<i
+										className={item.className}
+										style={{
+											fontSize: '24px',
+											color: '#ffffff',
+										}}
+									/>
+								</a>
+							</li>
+						))}
+				</ul>
+			</div>
+		</div>
+	</footer>
+)
+
+export default Footer
